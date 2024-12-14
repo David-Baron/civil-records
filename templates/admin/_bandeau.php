eb1ff69c7cc575325a30bb367271a7c823daae03
David-Baron <davidbaron@mycodingsoft.com>
2024-12-14 12:03:56 +0100

    Fix vendor

---
+++ templates/admin/_bandeau.php
@@ -0,0 +1,4 @@
+<div class="bandeau">
+    <strong><a href="<?= SITE_URL ?>"><?= SITENAME ?></a> || </strong>
+    Dépouillement de tables et actes d'état-civil ou de registres paroissiaux
+</div>
---
+++ templates/admin/_commentaire.php
@@ -0,0 +1,9 @@
+<p>
+    La consultation des tables et des actes présentés sur ce site est libre et gratuite.
+    <br>
+    Ces données sont mises à disposition pour faciliter les recherches des généalogistes
+    amateurs.
+    <br>
+    Il est interdit de faire un usage commercial de ces données ou de les redistribuer sur CD ou tout autre
+    support.
+</p>
